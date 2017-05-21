bfb3ae6c1307d832025b5b7614094155be0bb1ba
mohamed <msalad@uncc.edu>
2017-05-20 20:24:45 -0400

    Added URL parsing, didSet which will trigger when the url is changed

--- Cassini/Cassini/ImageViewController.swift
+++ Cassini/Cassini/ImageViewController.swift
@@ -9,27 +9,43 @@
 import UIKit
 
 class ImageViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
     
 
-    /*
-    // MARK: - Navigation
+    
+    var image: UIImage? = nil
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        imageURL = URL(string: "http://stanford.edu/about/images/intro_about.jpg")
     }
-    */
+    var imageURL: URL? {
+        //Didset represents what happens
+        //When somebody sets the URL
+        didSet {
+            if(view.window != nil){
+            fetchUrl()
+                imageV.image = image
+            }
+            
+        }
+    }
+    @IBOutlet weak var imageV: UIImageView!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+    }
+    func fetchUrl(){
+        if let url = imageURL {
+            let urlContents =
+                
+                try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                image = UIImage(data :imageData)!
+            }
+        }
+        
+    }
 
 }
